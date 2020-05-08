dc2a3dc3c891ea544a1c190d50404b58af756dc8
Magnus Spånggård <magnus@technocreatives.com>
2020-05-08 17:05:35 +0200

    trimming signal and message that otherwise can result in undefined prop values if a whitespace sneaks into the message or signal defenition.

--- can/can-listen.js
+++ can/can-listen.js
@@ -1,37 +1,37 @@
-module.exports = function(RED) {
-    function CanListenNode(config) {
-        RED.nodes.createNode(this, config);
+module.exports = function (RED) {
+  function CanListenNode(config) {
+    RED.nodes.createNode(this, config);
 
-        this.can = config.can;
-        this.canConnection = RED.nodes.getNode(this.can);
-        this.message = config.message;
-        this.signal = config.signal;
-        this.name = config.name;
-        this.onupdate = config.onupdate;
+    this.can = config.can;
+    this.canConnection = RED.nodes.getNode(this.can);
+    this.message = config.message.trim();
+    this.signal = config.signal.trim();
+    this.name = config.name;
+    this.onupdate = config.onupdate;
 
-        var node = this;
+    var node = this;
 
-        // no can no action
-        if (!this.canConnection) {
-            this.error(RED._('can.errors.missing-config'));
-            return;
-        }
-
-        this.update = function(value) {
-            node.send({
-                payload: value
-            });
-        };
-
-        node.canConnection.register(this);
-
-        this.on('close', function(done) {
-            node.canConnection.deregister(node, function() {
-                var name = node.name === '' ? node.id : node.name;
-                node.log('Node ' + name+ ' was deregistered.');
-                done();
-            });
-        });
+    // no can no action
+    if (!this.canConnection) {
+      this.error(RED._("can.errors.missing-config"));
+      return;
     }
-    RED.nodes.registerType('can-listen', CanListenNode);
+
+    this.update = function (value) {
+      node.send({
+        payload: value,
+      });
+    };
+
+    node.canConnection.register(this);
+
+    this.on("close", function (done) {
+      node.canConnection.deregister(node, function () {
+        var name = node.name === "" ? node.id : node.name;
+        node.log("Node " + name + " was deregistered.");
+        done();
+      });
+    });
+  }
+  RED.nodes.registerType("can-listen", CanListenNode);
 };
